5e1af3b0c7858484f9d28460a474bb2c51ec5468
Jaime Villarreal <jaime.villarreal@wizeline.com>
2022-03-18 16:34:02 -0600

    Added header component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
 
 function App() {
@@ -7,22 +7,7 @@ function App() {
   console.log(data, isLoading);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header/>
   );
 }
 
