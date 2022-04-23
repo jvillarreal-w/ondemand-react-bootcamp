e57750394375d9c1cfee5cc67d206182e803cd32
Jaime Villarreal <jaime.villarreal@wizeline.com>
2022-04-23 15:17:05 -0500

    [D2 Bonus] Added loading screen.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/Content/Home/HomePage";
 import ProductList from "./components/Content/Products/ProductList";
+import Loader from "./components/Content/Loader/Loader";
 import { useState } from "react";
 
 function App() {
   let [productListToggle, setProductListToggle] = useState(true);
 
+  function sleep(time){
+    return new Promise((resolve)=>setTimeout(resolve,time)
+  )
+}
+
   const handleToggle = () => {
-    return setProductListToggle(!productListToggle);
+    document.querySelector(".App").classList.add('loading');
+    sleep(2000).then(()=>{
+      document.querySelector(".App").classList.remove('loading');
+      return setProductListToggle(!productListToggle);
+   })
+    
   }
 
   const handleShowHomePage = () => {
@@ -26,6 +37,7 @@ function App() {
         <button onClick={() => handleToggle()}>View all products</button>
       </div>
       <Footer/>
+      <Loader/>
     </div>
   )
 }
---
+++ src/components/Content/Loader/Loader.js
@@ -0,0 +1,10 @@
+import "./Loader.css";
+
+const Loader = () => {
+    return (
+        <div className="loading-modal">
+        </div>
+    )
+}
+
+export default Loader;
